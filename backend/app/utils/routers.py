eb15952638335645eaeba4fd519744d8571dc740
Aliaksei Yafremau <447654972g@gmail.com>
2024-12-26 01:42:33 +0000

    Add '/api' to routers

--- backend/app/utils/routers.py
+++ backend/app/utils/routers.py
@@ -5,9 +5,9 @@ from app.routers.invitation_router import candidate_router
 from app.routers.statistics_router import statistics_router
 
 
-def register_routers(app):
-    app.include_router(manager_router)
-    app.include_router(auth_router)
-    app.include_router(candidate_router)
-    app.include_router(photo_router)
-    app.include_router(statistics_router)
+def register_routers(app, prefix: str = "/api"):
+    app.include_router(manager_router, prefix=prefix)
+    app.include_router(auth_router, prefix=prefix)
+    app.include_router(candidate_router, prefix=prefix)
+    app.include_router(photo_router, prefix=prefix)
+    app.include_router(statistics_router, prefix=prefix)
